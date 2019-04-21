cb0da27b5bb3517ea15a8af47d4182517f5b4395
unknown <archivecoderzero@gmail.com>
2019-04-21 12:54:02 -0700

    Update 04/21/2019, See readME

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 ### this is my Theme of the Homework that was given to us , a hacker theme where you guess people famous people password . The clue is the "Person you are hacking" ie ,"Tim Cook" , "Elon Musk" or "Barrack Obama" , the answers are words related to them , in this case the answers are "apple" (because Tim Cook founded Apple) , "tesla" because Elon Musk is the CEO of Tesla and "change" because of Obama's famous campaign line "CHANGE" . There are many nuances in the page ,3 worth mentioning (i spent hours figuring it out ) namely the "username" where it actually displays the persons name on Log-in and the "password" where it will emulate someone putting in a password thru the password form , and lastly , look for the "BONUS" word because it gives you 5 points...oh and one last thing......HAVE FUN!!!
 
-##Version 0.1
+## Version 0.1
 >psuedocoded and researched the net to have an insight on how to tackle this problem 
 
 
-##Version 0.5
+## Version 0.5
 >coded basic HTML layout and theme
 
 
@@ -17,7 +17,7 @@
 
 >Added animation for the "password" form , so it looks like you are puting a real password on it, **check it out!**
 
-##Version 1.5
+## Version 1.5
 >Implemented overflow hidden in HTML so that the animation wont push the navbar header . 
 
 >Minor cosmetic changes 
@@ -26,7 +26,7 @@
  
 >Minor word and clue changes .
 
-##Version 1.6
+## Version 1.6
 > Fixed footer to stick to the bottom , when screen size is changed.
 
 > Fixed short names showing the underlying element of the "clue1" on html file
@@ -37,7 +37,17 @@
 
 > on later versions, i will add a function to not repick the same word 
 
-### NOTE : No copyright, feel free to change or improve this "framework" , 
+
+## Version 2.0 04/21/19
+> Added a function so that it would not repeat words Over and over and over again , once you guessed the password, you wont have to guess it again.
+
+> Replaced some passwords..
+
+> minor cosmetic updates
+
+> check out the Console to see how the unRepeat function works . cuz i think its cool ... (im proud of it :)
+
+### NOTE : No copyright, feel free to change or improve this "framework" fellow "hackers" :) , 
 
 
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 //Variables for the game :
 
 //
-var library = ["neo","windows", "tesla","password","metalgear","apple" ,"victorious" ,"change","golf","qwerty" ,"bonus" , "king" , "breakup" ];
-var namer = ["Morpheus" , "Cypher" , "Oracle" , "Neo" , "Trinity" , "Hackerman" , "The Boss","The Techlead", "Techies", "Icedfrog", "Iron Man"]
-var clueUser = ["Keanu Reeves" ,"Bill  Gates" , "Elon  Musk" , "Mark Zuckerburg", "Hideo Kojima" , "Tim D Cook" , "Ariana Grande", "Barrack Obama" , "Tiger Woods" , "Donald Trump","John R Chavez" , "Elvis Presley" , "Taylor Swift"]
+var library = ["neo", "windows", "tesla", "password", "metalgear", "apple", "victorious", "hollywood", "golf", "legend", "bonus", "king", "breakup"];
+var namer = ["Morpheus", "Cypher", "Oracle", "Neo", "Trinity", "Hackerman", "The Boss", "The Techlead", "Techies", "Icedfrog", "Iron Man"]
+var clueUser = ["Keanu Reeves", "Bill  Gates", "Elon  Musk", "Mark Zuckerburg", "Hideo Kojima", "Tim D Cook", "Ariana Grande", "Kim Kardashian", "Tiger Woods", "Will Smith", "John R Chavez", "Elvis Presley", "Taylor Swift"]
 var asterisk = [];
 var lettersInPicked = [];
 var pickedWord = [];
@@ -13,7 +13,7 @@ var wrongPicks = [];
 var wins = 0;
 var loss = 0;
 var picksLeft = 10;
-
+var picked = [];
 
 
 //FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:FUNCTIONS HERE:
@@ -43,8 +43,8 @@ function startGame() {
     numberOfAster = lettersInPicked.length;
 
     //Populate number of blanks with * based on how many letters in pickedWord
-    for (var i = 0; i < numberOfAster; i++) 
-    
+    for (var i = 0; i < numberOfAster; i++)
+
     //For every iteration when counter "i" is less than number of blanks of the picked word, create an asterisk
     {
         asterCorrectPicks.push("*");
@@ -60,113 +60,144 @@ function startGame() {
     document.getElementById("cluePerson").innerHTML = clue;
     document.getElementById("cluePerson1").innerHTML = clue;
 
-    
-//END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTIONEND OF GAMESTART FUNCTION
+
+    // this function enables the the non repeating of the words, if you successfully guessed the 13 words, it will trigger a restart page 
+    function unRepeat() {
+        console.log(picked.length);
+        console.log(library.length);
+        for (i = 0; i < library.length; i++) {
+            //       console.log("unRepeater is Functioning");
+            //       console.log(pickedWord + " unrepeater pickedWord");
+            //      console.log(picked[i] +  " unrepeater ARRAY picked");
+            if (picked[i] == pickedWord) {
+                console.log(picked[i] + " Picked zero unRepeater Triggerd");
+                console.log(pickedWord + " unRepeater Triggerd");
+                if (picked.length == library.length) {
+                    console.log(library.length + " RELOAD PROC");
+                    console.log(picked.length + " RELOAD PROC");
+                    alert("YOU HACKED EVERYONE! YOU WIN THE INTERNET!");
+                    location.reload();
+                }
+                else {
+                    startGame();
+                    console.log(" ELSE PROCCING")
+                }
+            }
+        }
+    }
+    /// END OF UNREPEAT FUNCTION
+
+    unRepeat();
+    //END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTION END OF GAMESTART FUNCTIONEND OF GAMESTART FUNCTION
 }
-
-
 
 
 /////////
 
-function bonusChecker() { 
 
-    var bonusWord = asterCorrectPicks.join("") 
-    console.log(asterCorrectPicks.join("") )
-// if the random word is the "bonus word " , the user wins 10 points
+function bonusChecker() {
+
+    var bonusWord = asterCorrectPicks.join("")
+    // console.log(asterCorrectPicks.join(""))
+    // if the random word is the "bonus word " , the user wins 10 points
     if ("bonus" === bonusWord || "neo" === bonusWord) {
-            wins+10;
-    //alert BONUS TRIGGERD
-            alert("BONUS TRIGGERED!")
-    //Update the win counter in HTML
-            document.getElementById("winCounter").innerHTML = wins;
-    //Rerun startgame function
-            startGame();
-        }
-    
-    //if the bonus checker did not proc , run the round checker function
-        else  {
-            roundChecker();
-        }
+        wins += 10;
+        //alert BONUS TRIGGERD
+        alert("BONUS TRIGGERED!");
+        picked.push(pickedWord);
+        // console.log (picked);
+        //Update the win counter in HTML
+        document.getElementById("winCounter").innerHTML = wins;
+        //Rerun startgame function
+        startGame();
     }
+
+    //if the bonus checker did not proc , run the round checker function
+    else {
+        roundChecker();
+    }
+}
 
 ///////
 
 // create a function to check when the user gives out an event "onkeyup" , a function checks if that (event) letter is matches a letter that is on the pickedWord
 function checker(letter) {
 
-//create a variable outside the for loop , set that variable to a bolean false by default.
+    //create a variable outside the for loop , set that variable to a bolean false by default.
     var isLetterInWord = false;
 
-//using the numberOfAster variable , make a forloop to check the letters one by one on the pickedWord.
+    //using the numberOfAster variable , make a forloop to check the letters one by one on the pickedWord.
     for (var i = 0; i < numberOfAster; i++) {
 
-//using if statement , if the one of the index of the pickedWord matches with the letter, set the bolean to True , to be used later.
+        //using if statement , if the one of the index of the pickedWord matches with the letter, set the bolean to True , to be used later.
         if (pickedWord[i] == letter) {
             isLetterInWord = true;
         }
 
-//END OF FORLOOP END OF FORLOOP //END OF FORLOOP END OF FORLOOP //END OF FORLOOP END OF FORLOOP
+        //END OF FORLOOP END OF FORLOOP //END OF FORLOOP END OF FORLOOP //END OF FORLOOP END OF FORLOOP
     }
 
-//when isLetterIN
+    //when isLetterIN
     if (isLetterInWord) {
-//same as above, using the number of blanks , run thru the pickedWord's index, 
+        //same as above, using the number of blanks , run thru the pickedWord's index, 
         for (var z = 0; z < numberOfAster; z++) {
 
-//if statement of if pickedWord's index matches the letter , put the letter to the index of the pickedWord, populate it with the letter
+            //if statement of if pickedWord's index matches the letter , put the letter to the index of the pickedWord, populate it with the letter
             if (pickedWord[z] == letter) {
                 asterCorrectPicks[z] = letter;
             }
         }
     }
-//else, decrement picks left then push the wrong pick to the wrongPicks array
+    //else, decrement picks left then push the wrong pick to the wrongPicks array
     else {
- //decrement the picks left 
+        //decrement the picks left 
         picksLeft--;
         wrongPicks.push(letter);
     }
-//END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER
-    
+    //END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER FUNCTION END OF CHECKER
+
 }
 
 
-
 //create a function to determine if the user wins or loss already
-function roundChecker() { 
+function roundChecker() {
 
-//change the array lettersInPicked to a string , match that to blanksCorrectPick change that to string , if its equal, increment wins variable.   
+    //change the array lettersInPicked to a string , match that to blanksCorrectPick change that to string , if its equal, increment wins variable.   
     if (lettersInPicked.toString() == asterCorrectPicks.toString()) {
-    console.log ("test1" + lettersInPicked.toString())
-    console.log ("test2" + asterCorrectPicks.toString())
+        // console.log("test1" + lettersInPicked.toString())
+        // console.log("test2" + asterCorrectPicks.toString())
         wins++;
-// Allert message that you win 
-        alert("You Successfully Hacked " + clue )
-//Update the win counter in HTML
+        picked.push(pickedWord);
+        // console.log(picked);
+        // Allert message that you win 
+        alert("You Successfully Hacked " + clue)
+        //Update the win counter in HTML
         document.getElementById("winCounter").innerHTML = wins;
-//Rerun startgame function
+        //Rerun startgame function
         startGame();
     }
 
-//If picks left is equal to zero, alert you lose
+    //If picks left is equal to zero, alert you lose
     else if (picksLeft == 0) {
 
         // added game mechanic , when user mis picks a word , it deducts a point from their wins , if the wins is > 0
-        if (wins>0){
-        wins--;
-        loss++}
+        if (wins > 0) {
+            wins--;
+            loss++
+        }
 
-        else{
-        loss++}
+        else {
+            loss++
+        }
 
-        alert("You Failed to hack " + clue );
+        alert("You Failed to hack " + clue);
 
         document.getElementById("lossCounter").innerHTML = loss;
-//Rerun startgame function
+        //Rerun startgame function
         startGame();
     }
 
-//Reset the html back to gamestart variables
+    //Reset the html back to gamestart variables
     document.getElementById("attemptsLeft").innerHTML = picksLeft;
     document.getElementById("passWord").innerHTML = asterCorrectPicks.join("");
     document.getElementById("passWord1").innerHTML = asterisk.join("");
@@ -179,6 +210,7 @@ var person = prompt("Please Enter Your Username");
 randomPrefix = namer[Math.floor(Math.random() * namer.length)];
 document.getElementById("userName").innerHTML = person + " codename : " + randomPrefix;
 
+
 startGame();
 
 //Detect the key press . add asterisk to emulate putting in a passworkd
@@ -187,4 +219,4 @@ document.onkeyup = function (event) {
     checker(letterGuessed);
     bonusChecker();
     asterisk.push("*");
-      }
+}
